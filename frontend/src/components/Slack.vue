8d526d3d4f3404a4fc3009db817fe38d6e74fd1d
Ben Fitzhardinge <ben@fitzhardinge.net>
2023-05-08 13:03:21 +0800

    remove placeholder

--- frontend/src/components/Slack.vue
+++ frontend/src/components/Slack.vue
@@ -25,7 +25,7 @@
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 text-left">Your email</label>
             <input type="email" name="email" v-model="email"
                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 w-full p-2.5"
-                   placeholder="name@gmail.com" required>
+                   required>
           </div>
           <button type="submit"
                   id="slackSubmit"
