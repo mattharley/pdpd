6ede4e939f3917a9db9933b5ab7694dfa888718c
Ben Fitzhardinge <ben@fitzhardinge.net>
2023-05-08 13:05:59 +0800

    change input type to text

--- frontend/src/components/Slack.vue
+++ frontend/src/components/Slack.vue
@@ -23,7 +23,7 @@
         <form @submit.prevent="handleSubmit">
           <div class="mb-4">
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 text-left">Your email</label>
-            <input type="email" name="email" v-model="email"
+            <input type="text" name="email" v-model="email"
                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 w-full p-2.5"
                    required>
           </div>
