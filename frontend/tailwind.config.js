01eee740441c7c7be77618b658b80dea213bb08a
Ben Fitzhardinge <ben@fitzhardinge.net>
2024-04-02 16:50:02 +0800

    Updated chat to discord

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,16 @@
+// /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pythonYellow': '#ffde57',
+        'pythonBlue': '#4584b6',
+        'discordPurple': '#5865F2',
+      },
+    },
   },
   variants: {
     extend: {},
