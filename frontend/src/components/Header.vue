48da8f3fa1ba47c1ecbe20feeacebac94df53bd1
Ben Fitzhardinge <ben@fitzhardinge.net>
2024-11-24 14:58:02 +1100

    updated text on the PythonWA Website

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -68,14 +68,17 @@
       <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
         <div class="text-center">
           <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span class="block xl:inline">Welcome to the</span>
+            <span class="block xl:inline">Welcome to </span>
             {{ ' ' }}
             <span class="block text-pythonBlue xl:inline">Python WA</span>
-            {{ ' ' }}
-            <span class="block xl:inline">website</span>
           </h1>
           <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-            The Meetup Group for Perth-fectionists with Deadlines
+            The Meetup Group for Perth-fectionists with Deadlines.
+          </p>
+          <p class="mt-3 max-w-md mx-auto text-base sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+            We are a diverse group of Python Enthusiasts, <br/>
+            and welcome Python users of all flavours and skill levels. <br/>
+            We typically meet on the first Thursday of each month (except January).
           </p>
           <div class="mt-5 max-w-lg mx-auto sm:flex sm:justify-center md:mt-8">
             <div class="rounded-md shadow">
